3c24069c896a31ede8000e9255b5da58293d57f0
Veli-Pekka Nurmi <ext.veli-pekka.nurmi@kpmg.fi>
2018-06-20 13:27:02 +0300

    innitial commit

---
+++ src/odd-man-out/OddManOut.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+
+public class OddManOut {
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);   
+
+    int rounds = Integer.parseInt(sc.nextLine());
+ 
+    for (int y = 1 ; rounds >= y ; y++) {
+    	
+    		int noUse = Integer.parseInt(sc.nextLine());
+			String line = sc.nextLine();	
+			List <String> lineList = Arrays.asList(line.split(" "));
+		
+			String single = "Case #" + y + ": ";
+			for(String i : lineList) {	
+				int count = (int) lineList
+						.stream()
+						.filter(p -> p.equals(i))
+						.count();
+				if (count == 1) {
+					single += i;
+				}
+			}
+			System.out.println(single);
+	    }
+    }
+}
---
+++ src/take-two-stones/TakeTwoStones.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class TakeTwoStones {
+
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int input = sc.nextInt();
+    System.out.println(winner(input));
+
+    }
+    
+    public static String winner (int value) {
+        
+       String winner = "";
+        if ((value%2) == 0 ) {
+            winner = "Bob";
+        } else {
+            winner = "Alice";
+        }
+        return winner;
+    }
+    
+}
